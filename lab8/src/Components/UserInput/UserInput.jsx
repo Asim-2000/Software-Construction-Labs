ec54cd1035af2f8e39beb31122c77564db37d6c2
Muhammad Asim <m.asimkhaskheli@gmail.com>
2021-04-12 14:11:50 +0500

    boilerplate

---
+++ lab8/src/Components/UserInput/UserInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserInput() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ lab8/src/Components/UserOutput/UserOutput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserOutput() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
