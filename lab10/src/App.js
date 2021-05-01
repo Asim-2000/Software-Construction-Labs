8bdeb20b28430ba6ad733d445f24565117e4d978
Muhammad Asim <m.asimkhaskheli@gmail.com>
2021-05-01 17:26:18 +0500

    👌 IMPROVE: HomePage Improved

--- lab10/src/App.js
+++ lab10/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact />
+          <Route path='/' exact component={Home} />
         </Switch>
       </Router>
     </>
---
+++ lab10/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.css'
+import HeroSection from '../../components/HeroSection/HeroSection'
+
+export default function Home() {
+  return (
+    <>
+      
+          <HeroSection/>
+          
+    </>
+  )
+}
