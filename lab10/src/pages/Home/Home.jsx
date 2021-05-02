c452f5feda3f4dd7407a83904093a06b4310fd02
Muhammad Asim <m.asimkhaskheli@gmail.com>
2021-05-02 23:19:31 +0500

    👌 IMPROVE:  Added Cards and CardsItem component

--- lab10/src/pages/Home/Home.jsx
+++ lab10/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../../App.css'
+import Cards from '../../components/Card/Cards'
 import HeroSection from '../../components/HeroSection/HeroSection'
 
 export default function Home() {
   return (
     <>
       
-          <HeroSection/>
+      <HeroSection />
+      <Cards/>
           
     </>
   )
