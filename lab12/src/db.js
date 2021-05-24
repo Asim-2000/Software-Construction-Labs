40d0c4594bc4c06cd4c62977bef07dc776a26e72
Muhammad Asim <m.asimkhaskheli@gmail.com>
2021-05-24 20:25:28 +0500

    🐛 FIX: Moved schema.graphql outside src

--- lab12/src/db.js
+++ lab12/src/db.js
@@ -1,6 +1,6 @@
 const { DataStore } = require('notarealdb');
 
-const store = new DataStore('./data');
+const store = new DataStore('../data');
 
 module.exports = {
    students:store.collection('students')
